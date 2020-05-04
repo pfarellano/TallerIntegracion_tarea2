10c48216f7b4f9569ca351baf3b7ed6e9e196541
pfarellano <pfarellano@uc.cl>
2020-05-04 15:48:54 -0400

    arreglo modelos

--- hamburgueseria/models.py
+++ hamburgueseria/models.py
@@ -3,17 +3,17 @@ from django.db import models
 
 class Ingrediente(models.Model):
     id = models.IntegerField
-    nombre = models.CharField(max_length=60)
-    descripcion = models.CharField(max_length=60)
+    nombre = models.CharField(max_length=200)
+    descripcion = models.CharField(max_length=200)
     def __str__(self):
         return self.nombre
 
 class Hamburguesa(models.Model):
     id = models.IntegerField
-    nombre = models.CharField(max_length=60)
+    nombre = models.CharField(max_length=200)
     precio = models.IntegerField(default=0, editable=True)
     descripcion = models.CharField(max_length=60)
-    imagen = models.CharField(max_length=60)
+    imagen = models.CharField(max_length=200)
     ingredientes_hamburguesa = models.ManyToManyField(Ingrediente)
     def __str__(self):
         return self.nombre
